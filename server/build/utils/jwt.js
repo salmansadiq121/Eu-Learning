b4ec130db04e13e8f31f5fab68f06fb9ded4336b
Salman Sadiq <155360332+salmansadiq121@users.noreply.github.com>
2024-07-03 07:10:39 +0500

    Update jwt.js

--- server/build/utils/jwt.js
+++ server/build/utils/jwt.js
@@ -16,14 +16,14 @@ exports.accessTokenOptions = {
     maxAge: accessTokenExpire * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "none",
-    // secure: true,
+    secure: true,
 };
 exports.refreshTokenOptions = {
     expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
     maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "none",
-    // secure: true,
+    secure: true,
 };
 const sendToken = (user, statusCode, res) => {
     const accessToken = user.SignAccessToken();
